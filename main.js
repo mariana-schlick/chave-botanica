3dbf6c6e87735c025533cecdb98887261c3723d0
mariana-schlick <mariana.schlick@gmail.com>
2025-01-21 14:44:13 -0300

    Correção troca var por let

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function scrollToTop() {
 
 // Exibe o botão quando o usuário rolar para baixo
 window.onscroll = function() {
-    var button = document.getElementById("scrollToTopBtn");
+    let button = document.getElementById("scrollToTopBtn");
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
         button.style.display = "block"; // Mostra o botão
     } else {
